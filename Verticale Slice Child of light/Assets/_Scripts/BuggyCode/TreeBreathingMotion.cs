acb1287453cb7ca404bb1ac715703b67300810a6
kil98q <killiandekok@gmail.com>
2016-11-23 12:17:58 +0100

    Tree wiggle + particle

---
+++ Verticale Slice Child of light/Assets/_Scripts/BuggyCode/TreeBreathingMotion.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeBreathingMotion : MonoBehaviour {
+    [SerializeField]
+    float degreesBreathing = 10;
+    [SerializeField]
+    float startingRotation;
+    [SerializeField]
+    float degrees = 0;
+    [SerializeField]
+    bool RotationRight = true;
+    [SerializeField]
+    float startTime;
+    [SerializeField]
+    float duration;
+    // Use this for initialization
+    void Start () {
+        startingRotation = transform.localEulerAngles.z;
+        startTime = Time.time - Random.Range(1,15);
+        duration = Random.Range(40,50);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (RotationRight)
+        {
+            transform.localEulerAngles = new Vector3(0, 0, Damping(transform.localEulerAngles.z, startingRotation + degreesBreathing, duration));
+            if (Mathf.Abs(transform.localEulerAngles.z) > Mathf.Abs(startingRotation+ degreesBreathing) - 0.5)
+            {
+                startTime = Time.time;
+                RotationRight = !RotationRight;
+            }
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(0, 0, Damping(transform.localEulerAngles.z, startingRotation, duration));
+            if (Mathf.Abs(transform.localEulerAngles.z) < Mathf.Abs(startingRotation)+0.5)
+            {
+                startTime = Time.time;
+                RotationRight = !RotationRight;
+            }
+        }
+	}
+    float Damping(float currentRotation,float Target,float Speed)
+    {
+        return Mathf.SmoothStep(currentRotation, Target, (Time.time - startTime) / Speed);
+    }
+    
+}
--- Verticale Slice Child of light/Assets/_Scripts/ParticleMovement.cs
+++ Verticale Slice Child of light/Assets/_Scripts/ParticleMovement.cs
@@ -2,17 +2,41 @@
 using System.Collections;
 
 public class ParticleMovement : MonoBehaviour {
-    Vector2 Velocity = new Vector2(0,1);
+    [SerializeField]
+    float direction;
+    [SerializeField]
+    float velocity;
+    [SerializeField]
+    float vel;
+    [SerializeField]
+    float speed;
     [SerializeField] Rigidbody2D Particle;
 	// Use this for initialization
 	void Start () {
         Particle = GetComponent<Rigidbody2D>();
-        Particle.velocity = Velocity;
-        Particle.angularVelocity = 1;
-	}
+        StartCoroutine(AutoRotate());
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0,0, transform.localEulerAngles.x + Random.Range(-10, 10)));
-	}
+        velocity = Particle.velocity.x + Particle.velocity.y;
+        vel = Mathf.SmoothDamp(vel, speed, ref velocity, speed/2, 0.5f);
+        if(vel > 0.6)
+        {
+            vel = 0;
+        }
+        Particle.velocity = transform.up * vel;//Mathf.SmoothDamp(velocity, speed*20, ref velocity, Time.deltaTime * 6, 100); ;
+        Particle.rotation = Mathf.SmoothDamp(Particle.rotation, direction, ref velocity, speed,100);
+        
+    }
+    IEnumerator AutoRotate(){
+        while (true)
+        {
+            speed = Random.Range(0.1f, 0.5f);
+            direction = Random.Range(0f, 360f);
+            yield return new WaitForSeconds(3);
+        }
+        
+    }
 }
