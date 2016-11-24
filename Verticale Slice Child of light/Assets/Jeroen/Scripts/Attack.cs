b0c2f80680bc1e7a67b87d9018f9d391a42b385c
JherSter <jeroenavis123@gmail.com>
2016-11-24 12:14:38 +0100

    WeaponSwitching

---
+++ Verticale Slice Child of light/Assets/Jeroen/Scripts/Attack.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public enum WeaponTypes{
+	Normal = 0,
+
+}
+public class Attack : MonoBehaviour {
+	[SerializeField]
+	private Animator Anim;
+	[SerializeField]
+	private WeaponTypes weaponType;
+	[SerializeField]
+	private int Damage = 10;
+
+	private EnemyHealth _EnemyHealth;
+	// Use this for initialization
+	void Start () {
+		_EnemyHealth = GameObject.FindGameObjectWithTag ("Enemy").GetComponent<EnemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			switch (weaponType) {
+			case WeaponTypes.Normal:
+				NormalAtt ();
+				break;
+	
+
+			}
+		}
+
+			//Anim.Play ();
+		
+	
+	}
+	void NormalAtt(){
+		Debug.Log ("NormalAtt");
+	}
+
+
+}
+
+
