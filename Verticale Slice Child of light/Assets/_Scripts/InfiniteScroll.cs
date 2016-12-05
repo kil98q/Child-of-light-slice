446390a16f32b662876ce1e7a32c63e16c07df10
kil98q <killiandekok@gmail.com>
2016-12-05 16:11:26 +0100

    Small changes

---
+++ Verticale Slice Child of light/Assets/_Scripts/InfiniteScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfiniteScroll : MonoBehaviour {
+    public GameObject Player;
+    public Renderer rend;
+    public Camera Cam;
+    public bool Manual;
+    public float scrollSpeed;
+	void Start () {
+        //transform.localScale = new Vector3(Cam / 2 * (Screen.width / Screen.height), cam.orthographicSize / 2, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Manual)
+        {
+            scrollSpeed = Player.GetComponent<Rigidbody2D>().velocity.x / 100 * Time.timeScale;
+        }
+        
+        rend.material.mainTextureOffset = new Vector2(rend.material.mainTextureOffset.x + (scrollSpeed * Time.fixedDeltaTime),0);
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y,transform.position.z);
+	}
+}
---
+++ Verticale Slice Child of light/Assets/_Scripts/Particlespawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Particlespawner : MonoBehaviour {
+    public GameObject ParticlePrefab;
+    GameObject Spawner;
+    Vector3 SpawnerPos;
+    public int ParticleAmount;
+    public Vector3 SpawnRange;
+    List<GameObject> Particles = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        SpawnerPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RemoveParticlesOffscreen();
+        if(Particles.Count < ParticleAmount)
+        {
+            SpawnParticle();
+        }
+	}
+
+    void SpawnParticle()
+    {
+        GameObject NewParticle = Instantiate(ParticlePrefab);
+        Particles.Add(NewParticle);
+        NewParticle.transform.position = transform.position;
+    }
+    void RemoveParticlesOffscreen()
+    {
+        foreach(GameObject particlee in Particles)
+        {
+            /*
+            if (particlee != null)
+            {
+                Vector3 _partPos = new Vector3(particlee.transform.position.x, particlee.transform.position.y, particlee.transform.position.z);
+                print("Delete!");
+                if (_partPos.x > SpawnerPos.x / 2 || _partPos.x < -SpawnerPos.x / 2)
+                {
+                    DestroyImmediate(particlee);
+                }
+                if (_partPos.x > SpawnerPos.y / 2 || _partPos.y < -SpawnerPos.y / 2)
+                {
+                    DestroyImmediate(particlee);
+                }
+                if (_partPos.x > SpawnerPos.z / 2 || _partPos.z < -SpawnerPos.z / 2)
+                {
+                    DestroyImmediate(particlee);
+                }
+            }
+            */
+        }
+    }
+    
+}
