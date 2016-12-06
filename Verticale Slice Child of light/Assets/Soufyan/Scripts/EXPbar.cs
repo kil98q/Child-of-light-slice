955d0de49a94952f0da2a490b4738c54d26626df
soufyanhassan <18358@ma-web.nl>
2016-12-06 10:12:12 +0100

    EXP bar

---
+++ Verticale Slice Child of light/Assets/Soufyan/Scripts/EXPbar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EXPbar : MonoBehaviour
+{
+    [SerializeField] private Image _filledBar;
+    [SerializeField] private float _fillSpeed;
+    private float _amount = 0.01f;
+    
+	// Update is called once per frame
+	void Update()
+    {
+        _filledBar.fillAmount = Time.time * _fillSpeed;
+        _filledBar.fillAmount += _amount * _fillSpeed;
+
+        if(_filledBar.fillAmount == 0.35f)
+        {
+            _filledBar.fillAmount += 0.55f;
+        }
+        else if(_filledBar.fillAmount == 1f)
+        {
+            _filledBar.fillAmount -= 0.1f;
+        }
+
+	}
+}
